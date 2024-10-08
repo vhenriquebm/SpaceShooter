d11606bf27b582f57a47a19fce443e8c0d82b5a5
vhenriquebm <vhenriquebm@gmail.com>
2024-10-08 20:58:53 -0300

    Enemy script and functions for death and damage created

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamege(float dmg)
+    {
+        HurtSequence();
+
+        if (health <= 0)
+        {
+            DeathSequence();
+        }
+    }
+
+    public virtual void HurtSequence()
+    {
+
+    }
+
+    public virtual void DeathSequence()
+    {
+
+    }
+}
