ff20201048b61a795046adaa549c5cc3c30a4858
vhenriquebm <vhenriquebm@gmail.com>
2024-10-09 19:11:05 -0300

    Prefabs created for meteors

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] protected float health;
+    [SerializeField] protected Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Enemy/Meteor.cs
+++ Assets/Scripts/Enemy/Meteor.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Meteor : Enemy
 {
+
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    private float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(minSpeed, maxSpeed);
+        rb.velocity = Vector2.down * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void HurtSequence()
+    {
+    }
+
+    public override void DeathSequence()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            Destroy(otherCollider.gameObject);
+        }
     }
 }
